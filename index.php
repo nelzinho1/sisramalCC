b47b6a7fad3b0423b9c66ff2616f9abaccd8a222
Emanuel F. Souza <emanuelsouza1991@hotmail.com>
2023-05-04 22:54:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ session_start();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login de ramais - CC</title>
+    <title>Login de ramais </title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="css/bulma.min.css" />
     <link rel="stylesheet" type="text/css" href="css/login.css">
